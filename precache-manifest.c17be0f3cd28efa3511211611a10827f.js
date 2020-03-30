3484b137d0b8c655858b72371296e9b5555c2cba
shreedhar desai <desaishreedhar09@gmail.com>
2020-03-30 21:24:15 +0530

    Updates

---
+++ precache-manifest.c17be0f3cd28efa3511211611a10827f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea748fd506db22f5470b4a06e920b4d",
+    "url": "/DemoApp/index.html"
+  },
+  {
+    "revision": "0f6b5d3505f3eeb9d9d2",
+    "url": "/DemoApp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4d5efbb86d64a3739ab2",
+    "url": "/DemoApp/static/css/main.bec0db72.chunk.css"
+  },
+  {
+    "revision": "0f6b5d3505f3eeb9d9d2",
+    "url": "/DemoApp/static/js/2.8d927291.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/DemoApp/static/js/2.8d927291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5efbb86d64a3739ab2",
+    "url": "/DemoApp/static/js/main.de82d1b1.chunk.js"
+  },
+  {
+    "revision": "2e2c1a8494e7b92a923e",
+    "url": "/DemoApp/static/js/runtime-main.56e5e7a4.js"
+  }
+]);
